93c520da7aa304d23b0d2ab0478ee0bf34252d30
carlacodes <carla.griffiths@gmail.com>
2018-02-12 20:25:40 +0000

    Creating a StoreVariables class to store all user data

--- JavaProjectComp210P/src/project/Project.java
+++ JavaProjectComp210P/src/project/Project.java
@@ -1,3 +1,5 @@
+package project;
+
 ///------------------------------------------------------------
 ///
 ///LOUIS NGUYEN CARLA GRIFFITHS COMP210P PROJECT, DELIVERABLE 1
@@ -8,7 +10,8 @@ public class Project
 {
 	public static void main(String[] args)
     {
-     	new RunApp();
-     	
+     	RunApp call = new RunApp();
+     	call.StartMenu();
     }
+    
 }
---
+++ JavaProjectComp210P/src/project/StoreVariables.java
@@ -0,0 +1,15 @@
+package project;
+
+public class StoreVariables {
+	 RunApp call0 = new RunApp();
+     int NumberOfMember= call0.CreateProjectNumberofMembers();
+     
+     
+     RunApp call2 = new RunApp();
+     String ProjectName = call2.CreateProjectTitle();
+    // System.out.println(call2.CreateProjectTitle());
+     
+     RunApp call3 = new RunApp();
+     String[] TeamMember = call3.CreateProjectNamesofMembers(); 
+     //System.out.println(call3.CreateProjectNamesofMembers());
+}
